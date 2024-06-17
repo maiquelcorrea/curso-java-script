b0532e834683e3b5ae4cd8031cb85459a55f1486
maiquel <maiquel.correa12@gmail.com>
2024-06-17 16:28:14 -0300

    Att

---
+++ modulo1/2 variaveis e consoantes/script.js
@@ -0,0 +1,34 @@
+// variaveis js
+// var = variavel global
+// let = variavel no escopo
+// const = constante
+
+// EX:
+var nome = 'maiquel';
+
+console.log(nome);
+
+var nome = 'natalia';
+
+console.log(nome);
+
+{
+    let idade = 18; 
+}
+
+// console.log(idade);
+// nao funcionara pq let só funciona dentro do escopo
+
+// constante nao pode ser alterada
+const name = 'maiquel';
+
+var numero = 18;
+var nome2 = 'heloiza';
+var bol = true;
+var bol2 = false;
+
+console.log(true);
+console.log(bol);
+
+console.log(name+", tem "+numero+" anos");
+
---
+++ modulo1/3 Arrays/script.js
@@ -0,0 +1,22 @@
+// arrays 
+var nome1 = 'maiquel';
+var nome2 = 'natalia';
+var nome2 = 'fernando';
+
+var nomes = ['maiquel', 'natalia','heloiza'];
+
+console.log(nomes[1]);
+
+// outra forma de declarar um array
+
+var nomes2 = new Array('fernando','natalia','maiquel');
+// alterando nomes que estao dentro do array
+nomes2[0] = 'heloiza';
+
+console.log(nomes2[0]);
+
+// adicionando itens no array dps de ja ter o declarado
+
+nomes2.push('arthur');
+
+console.log(nomes2[3]);
